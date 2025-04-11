dbc87150ce3d9001909d449ca79d54721452a0ca
Rafael Medeiros <faeltadeu@hotmail.com>
2025-04-11 17:02:54 -0300

    Criação do site

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  imports: [],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+  contactEmail ='contato@medeiroscontabilidade.com';
+  contactPhone ='(31) 3224-8916';
+  contactAddress ='Rua da Bahia, 573 - Centro, Belo Horizonte - MG';
+}
