b528450888bb7ae3e0ece9c56d21157f888e2da4
Rafael Medeiros <faeltadeu@hotmail.com>
2025-04-11 17:37:23 -0300

    Ajuste no script de deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "🔧 Construindo o projeto Angular para produção..."
-ng build --configuration production --base-href /contabilidade-medeiros/
+ng build --configuration production --base-href /contMedeiros/
 
 echo "🧹 Limpando a pasta docs antiga..."
 rm -rf docs
 mkdir docs
 
 echo "📁 Copiando arquivos de build para a pasta docs..."
-cp -r dist/contabilidade-medeiros/browser/* docs/
+cp -r dist/contMedeiros/browser/* docs/
 
 echo "📦 Adicionando arquivos ao Git..."
 git add docs -f
