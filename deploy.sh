8c994f58b8133c44727dedebefaf0e33850826e3
Rafael Medeiros <faeltadeu@hotmail.com>
2025-04-12 15:51:45 -0300

    Atualizando script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-set -e
-
 echo "🔧 Construindo o projeto Angular para produção..."
-ng build --base-href "/contabilidade-medeiros/"
-touch dist/contabilidade-medeiros/.nojekyll
+ng build --configuration production --base-href /contMedeiros/
 
 echo "🧹 Limpando a pasta docs antiga..."
 rm -rf docs
 mkdir docs
 
 echo "📁 Copiando arquivos de build para a pasta docs..."
-cp -r dist/contabilidade-medeiros/browser/* docs/
+cp -r dist/contMedeiros/browser/* docs/
 
 echo "📦 Adicionando arquivos ao Git..."
 git add docs -f
